682d12e6a1a0c73181308d60b5bf69413c22fb6f
dev-suvi <suvarnamondal27@gmail.com>
2026-08-20 22:44:10 +0100

    Rename site title to BabyQ Top Secret

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css';
 
-export const metadata = { title: 'The Party Personality Test', description: 'A mysterious little party personality quiz.' };
+export const metadata = { title: 'BabyQ • Top Secret 🤫', description: 'A little BabyQ mystery. Your secret is waiting. 👀' };
 
 export default function RootLayout({ children }) {
   return <html lang="en"><body>{children}</body></html>;
