b11fa00fcfbf6eec09d10fc0cd022824812b2926
dev-suvi <suvarnamondal27@gmail.com>
2026-08-21 15:03:26 +0100

    Remove test email from OTP allowlist

--- app/api/auth/send-otp/route.js
+++ app/api/auth/send-otp/route.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis';
 import { randomInt, createHash } from 'crypto';
 
 const allowedEmails = new Set([
-'adamjones123@hotmail.co.uk','ashutosh.saxena23@gmail.com','faizamuskan.fm@gmail.com','gupta.sweta2403@gmail.com','kanishkakul0106@gmail.com','likhithagompa@gmail.com','namratagupta188@gmail.com','naveenmeher07@gmail.com','nikhiljain2112@gmail.com','nvbadrinarayanan@gmail.com','rahulaneja687@gmail.com','rohit3864@gmail.com','sayan.chanda.2020@gmail.com','shafeeq.rahman01@gmail.com','shivisaggi@gmail.com','vrindagrover10@gmail.com','shanky442@gmail.com','pragyarch@gmail.com','suvarnamondal27@gmail.com'
+'adamjones123@hotmail.co.uk','ashutosh.saxena23@gmail.com','faizamuskan.fm@gmail.com','gupta.sweta2403@gmail.com','kanishkakul0106@gmail.com','likhithagompa@gmail.com','namratagupta188@gmail.com','naveenmeher07@gmail.com','nikhiljain2112@gmail.com','nvbadrinarayanan@gmail.com','rahulaneja687@gmail.com','rohit3864@gmail.com','sayan.chanda.2020@gmail.com','shafeeq.rahman01@gmail.com','shivisaggi@gmail.com','vrindagrover10@gmail.com','shanky442@gmail.com','pragyarch@gmail.com'
 ]);
 let clientPromise;
 async function redis(){if(!clientPromise){const client=createClient({url:process.env.REDIS_URL});client.on('error',console.error);clientPromise=client.connect().then(()=>client).catch(e=>{clientPromise=undefined;throw e;});}return clientPromise;}
